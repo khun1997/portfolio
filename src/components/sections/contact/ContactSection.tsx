3a281d5a6b91cec90daf87f788057e7a95e1ea7c
khun1997 <kkhun531@gmail.com>
2024-12-09 02:19:43 +0700

    feat:added raw contact section and react-hook-form

--- src/components/sections/contact/ContactSection.tsx
+++ src/components/sections/contact/ContactSection.tsx
@@ -1,4 +1,5 @@
-import { MainButton } from "@/components/common/CommonButton";
+"use client";
+// import { MainButton } from "@/components/common/CommonButton";
 import PasswordTextField from "@/components/common/PasswordTextField";
 import {
   Stack,
@@ -9,6 +10,7 @@ import {
 } from "@mui/material";
 // import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import React from "react";
+// import { Controller } from "react-hook-form";
 
 const ContactSection = () => {
   const sm = useMediaQuery((theme: Theme) => theme.breakpoints.down("sm"));
@@ -63,14 +65,66 @@ const ContactSection = () => {
             Lets work together
           </Typography>
         </Stack>
-
         <Stack
           component="form"
           // onSubmit={handleSubmit(onSubmit)}
           flexGrow={1}
           mb={5}
           sx={{ gap: sm ? "23px" : "30px" }}
-        ></Stack>
+        >
+          <Stack
+            component="fieldset"
+            // disabled={isLoading}
+            spacing={2}
+            alignItems="center"
+            sx={{ border: 0 }}
+          >
+            {/* <Controller
+              name="email"
+              // control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <TextField
+                  label="အီးမေးလ်"
+                  required
+                  autoComplete="off"
+                  type="email"
+                  variant="outlined"
+                  fullWidth
+                  // InputProps={{ disabled: isLoading }}
+                  margin="normal"
+                  // error={!!errors.email}
+                  // helperText={errors.email?.message}
+                />
+              )}
+            />
+            <Controller
+              name="password"
+              // control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <PasswordTextField
+                  required
+                  autoComplete="off"
+                  label="စကားဝှက်"
+                  variant="outlined"
+                  fullWidth
+                  // error={!!errors.password}
+                  // helperText={errors.password?.message}
+                />
+              )}
+            /> */}
+
+            {/* <LoadingButton
+              disabled={!isValid}
+              type="submit"
+              variant="contained"
+              loading={isLoading}
+            >
+              အကောင့်ဝင်ရောက်မည်
+            </LoadingButton> */}
+          </Stack>
+        </Stack>
       </Stack>
 
       {/* <Stack sx={{ marginTop: "40px" }}>
